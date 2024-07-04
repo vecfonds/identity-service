a7afb755015f1c451c8a0f825efd10c62884cce7
vecfonds <dungsppkhdallss@gmail.com>
2024-07-04 22:50:52 +0700

    fix(UserCreationRequest): Minimum number of characters in field validation

--- src/main/java/com/vecfonds/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/vecfonds/identity_service/dto/request/UserCreationRequest.java
@@ -7,8 +7,9 @@ import java.time.LocalDate;
 
 @Data
 public class UserCreationRequest {
+    @Size(min = 5, message = "Username must be at least 5 characters!")
     private String username;
-    @Size(min = 5, message = "Password must be at least 8 characters!")
+    @Size(min = 5, message = "Password must be at least 5 characters!")
     private String password;
     private String firstName;
     private String lastName;
